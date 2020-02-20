ce7ad032df4c65cf036891d057656f088e9be41c
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-02-19 22:10:41 -0300

    Criação da tela e chamada dos Containers e Widget, mostando os gifs

--- Flutter/curso16apps/flutter_search_gifs/lib/ui/home_page.dart
+++ Flutter/curso16apps/flutter_search_gifs/lib/ui/home_page.dart
@@ -5,8 +5,8 @@ import 'dart:async';
 
 const url_melhores_gifs =
     "https://api.giphy.com/v1/gifs/trending?api_key=ETRUxGlwAKlCo4YAMLeY6R9HR2QebWrx&limit=25&rating=G";
-const url_pesquisa =
-    "https://api.giphy.com/v1/gifs/search?api_key=ETRUxGlwAKlCo4YAMLeY6R9HR2QebWrx&q=_buscar&limit=25&offset=_itenspage&rating=G&lang=pt";
+const url_imagem =
+    "https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif";
 
 class HomePage extends StatefulWidget {
   @override
@@ -22,9 +22,12 @@ class _HomePageState extends State<HomePage> {
     if (_search == null)
       response = await http.get(url_melhores_gifs);
     else {
-      _url_pesquisa_mont = url_pesquisa;
-      _url_pesquisa_mont.replaceAll("_buscar", _search);
-      _url_pesquisa_mont.replaceAll("_itenspage", _offSet.toString());
+      _url_pesquisa_mont =
+          "https://api.giphy.com/v1/gifs/search?api_key=ETRUxGlwAKlCo4YAMLeY6R9HR2QebWrx&q=$_search&limit=25&offset=$_offSet&rating=G&lang=pt";
+      ;
+
+      _search.isEmpty;
+      print(_url_pesquisa_mont);
       response = await http.get(_url_pesquisa_mont);
     }
     return json.decode(response.body);
@@ -40,6 +43,75 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network(url_imagem),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black12,
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  labelText: "Pesquise Aqui",
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder()),
+              style: TextStyle(color: Colors.white, fontSize: 18.0),
+              textAlign: TextAlign.center,
+              onSubmitted: (text) {
+                setState(() {
+                  _search = text;
+                  text.isEmpty;
+                });
+              },
+            ),
+          ),
+          //Ocupar todo o espaço do Builder
+          Expanded(
+            child: FutureBuilder(
+                future: _getGifs(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                    case ConnectionState.none:
+                      return Container(
+                        width: 200.0,
+                        height: 200.0,
+                        alignment: Alignment.center,
+                        child: CircularProgressIndicator(
+                            valueColor:
+                                AlwaysStoppedAnimation<Color>(Colors.white),
+                            strokeWidth: 5.0),
+                      );
+                    default:
+                      if (snapshot.hasError)
+                        return Container();
+                      else
+                        return _createGifTable(context, snapshot);
+                  }
+                }),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+        padding: EdgeInsets.all(10.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+        itemCount: snapshot.data["data"].length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+              child: Image.network(
+            snapshot.data["data"][index]["images"]["fixed_height_small"]["url"],
+            height: 100.0,
+            fit: BoxFit.cover,
+          ));
+        });
   }
 }
