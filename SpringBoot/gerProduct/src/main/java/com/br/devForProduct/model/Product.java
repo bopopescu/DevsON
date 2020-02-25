cc80624570d167b1e2c3696f931260b666c6b63c
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-02-25 20:16:18 -0300

    Alterado projeto para importado pelo start.spring.io para trazer as dependencias corretamente.

---
+++ SpringBoot/gerProduct/src/main/java/com/br/devForProduct/model/Product.java
@@ -0,0 +1,49 @@
+package com.br.devForProduct.model;
+
+public class Product {
+    private int id;
+    private String descricao;
+    private String marca;
+    private String numeroPatrimonio;
+
+    public Product(int id) {
+        this.id = id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setNumeroPatrimonio(String numeroPatrimonio) {
+        this.numeroPatrimonio = numeroPatrimonio;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getNumeroPatrimonio() {
+        return numeroPatrimonio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Product() {
+    }
+
+
+}
