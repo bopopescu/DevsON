1ef08a215622eb146fe495ee8588ac41761410e5
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-02-25 19:47:09 -0300

    Alterado anotação da classe, para que tenha o mesmo comportamento

--- SpringBoot/devForProduct/src/main/java/br/com/devForProduct/awesome/AplicationStarter.java
+++ SpringBoot/devForProduct/src/main/java/br/com/devForProduct/awesome/AplicationStarter.java
@@ -2,12 +2,11 @@ package br.com.devForProduct.awesome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@EnableAutoConfiguration
-@ComponentScan
-@Configuration
+@SpringBootApplication
 public class AplicationStarter {
     public static void main(String[] args) {
         SpringApplication.run(AplicationStarter.class,args);
