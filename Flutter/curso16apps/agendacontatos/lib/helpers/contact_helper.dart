0d4130dd845371dfe4edf0b200acf16f1d608c30
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-03-03 04:33:39 -0300

    Flutter Agenda de Contatos

---
+++ Flutter/curso16apps/agendacontatos/lib/helpers/contact_helper.dart
@@ -0,0 +1,38 @@
+import 'package:sqflite/sqflite.dart';
+
+final String idCol = "idCol";
+final String nomeCol = "nomeCol";
+final String emailCol = "emailCol";
+final String telefoneCol = "telefoneCol";
+final String imagemCol = "imagemCol";
+
+class contactHelper {}
+
+class Contact {
+  int id;
+  String nome;
+  String email;
+  String telefone;
+  String imagem;
+
+  Contact.fromMap(Map map) {
+    id = map[idCol];
+    nome = map[nomeCol];
+    email = map[emailCol];
+    telefone = map[telefoneCol];
+    imagem = map[imagemCol];
+  }
+
+  Map toMap() {
+    Map<String, dynamic> map = {
+      nomeCol: nome,
+      emailCol: email,
+      telefoneCol: telefone,
+      imagemCol: imagem
+    };
+    if (id != null) {
+      map[idCol] = id;
+    }
+    return map;
+  }
+}
