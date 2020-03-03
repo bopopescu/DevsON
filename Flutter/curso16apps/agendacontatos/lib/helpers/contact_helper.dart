7142098831532c44a08a5ba811b642d72a103bb4
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-03-03 04:53:42 -0300

    Dao Helper

--- Flutter/curso16apps/agendacontatos/lib/helpers/contact_helper.dart
+++ Flutter/curso16apps/agendacontatos/lib/helpers/contact_helper.dart
@@ -1,12 +1,47 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
+final String contactTable = "contactTable";
 final String idCol = "idCol";
 final String nomeCol = "nomeCol";
 final String emailCol = "emailCol";
 final String telefoneCol = "telefoneCol";
 final String imagemCol = "imagemCol";
 
-class contactHelper {}
+class ContactHelper {
+  //Singleton
+  final String sSqlCreate = "CREATE TABLE $contactTable("
+      "$idCol INTEGER PRIMARY KEY,"
+      "$nomeCol TEXT,"
+      "$emailCol TEXT,"
+      "$telefoneCol TEXT,"
+      "$imagemCol TEXT"
+      ")";
+  static final ContactHelper _instance = Contact.internal();
+
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  Database _db;
+  get db {
+    if (_db != null) {
+      return _db;
+    } else {
+      _db = initDb();
+    }
+  }
+
+  initDb() async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, "contact.db");
+
+    openDatabase(path, version: 1,
+        onCreate: (Database db, int newerVersion) async {
+      await db.execute(sSqlCreate);
+    });
+  }
+}
 
 class Contact {
   int id;
