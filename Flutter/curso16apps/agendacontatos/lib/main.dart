2afdd805da13d58f2e90fa4064dfba3ddefde8e5
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-03-04 01:49:40 -0300

    Dao atualizado e criação do HomePage

--- Flutter/curso16apps/agendacontatos/lib/main.dart
+++ Flutter/curso16apps/agendacontatos/lib/main.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:agendacontatos/ui/home_page.dart';;
 
-void main() => runApp(MaterialApp());
+void main() => runApp(MaterialApp(
+      home: HomePage(),
+    ));
