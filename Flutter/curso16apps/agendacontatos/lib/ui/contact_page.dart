8b2d46099ca6257fac82ccf9655860c5a62297c1
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-03-06 02:33:12 -0300

    Colocado todo o Scaffold no WillPopScope

--- Flutter/curso16apps/agendacontatos/lib/ui/contact_page.dart
+++ Flutter/curso16apps/agendacontatos/lib/ui/contact_page.dart
@@ -38,76 +38,81 @@ class _ContactPageState extends State<ContactPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text(_editedContact.nome ?? "Novo Contato"),
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (_editedContact.nome != null && _editedContact.nome.isNotEmpty) {
-            Navigator.pop(context, _editedContact);
-            //remove a tela e volta para a tela anterior. (PILHA)
-          } else {
-            FocusScope.of(context).requestFocus(_nameFocus);
-          }
-        },
-        child: Icon(Icons.save),
-        backgroundColor: Colors.red,
-      ),
-      body: new SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        padding: const EdgeInsets.all(10.0),
-        child: new Column(
-          children: <Widget>[
-            GestureDetector(
-              child: Container(
-                width: 140.0,
-                height: 140.0,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                      image: _editedContact.imagem != null
-                          ? FileImage(File(_editedContact.imagem))
-                          : AssetImage("images/person.png")),
+    return WillPopScope(
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.red,
+            title: Text(_editedContact.nome ?? "Novo Contato"),
+            centerTitle: true,
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              if (_editedContact.nome != null &&
+                  _editedContact.nome.isNotEmpty) {
+                Navigator.pop(context, _editedContact);
+                //remove a tela e volta para a tela anterior. (PILHA)
+              } else {
+                FocusScope.of(context).requestFocus(_nameFocus);
+              }
+            },
+            child: Icon(Icons.save),
+            backgroundColor: Colors.red,
+          ),
+          body: new SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            padding: const EdgeInsets.all(10.0),
+            child: new Column(
+              children: <Widget>[
+                GestureDetector(
+                  child: Container(
+                    width: 140.0,
+                    height: 140.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: _editedContact.imagem != null
+                              ? FileImage(File(_editedContact.imagem))
+                              : AssetImage("images/person.png")),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            new TextField(
-              controller: _nameController,
-              focusNode: _nameFocus,
-              decoration: InputDecoration(labelText: "Nome"),
-              onChanged: (text) {
-                _bUserEdit = true;
-                setState(() {
-                  _editedContact.nome = text;
-                });
-              },
-            ),
-            new TextField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: "e-mail"),
-              onChanged: (text) {
-                _bUserEdit = true;
+                new TextField(
+                  controller: _nameController,
+                  focusNode: _nameFocus,
+                  decoration: InputDecoration(labelText: "Nome"),
+                  onChanged: (text) {
+                    _bUserEdit = true;
+                    setState(() {
+                      _editedContact.nome = text;
+                    });
+                  },
+                ),
+                new TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(labelText: "e-mail"),
+                  onChanged: (text) {
+                    _bUserEdit = true;
 
-                _editedContact.nome = text;
-              },
-              keyboardType: TextInputType.emailAddress,
-            ),
-            new TextField(
-              controller: _phoneController,
-              decoration: InputDecoration(labelText: "telefone"),
-              onChanged: (text) {
-                _bUserEdit = true;
+                    _editedContact.nome = text;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                new TextField(
+                  controller: _phoneController,
+                  decoration: InputDecoration(labelText: "telefone"),
+                  onChanged: (text) {
+                    _bUserEdit = true;
 
-                _editedContact.nome = text;
-              },
-              keyboardType: TextInputType.phone,
+                    _editedContact.nome = text;
+                  },
+                  keyboardType: TextInputType.phone,
+                ),
+              ],
             ),
-          ],
+          ),
         ),
-      ),
-    );
+        onWillPop: _requestPop);
   }
+
+  _requestPop() {}
 }
