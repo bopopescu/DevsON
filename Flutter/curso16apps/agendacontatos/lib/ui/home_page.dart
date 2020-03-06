d448e49551e6694295e4ab903ade618127ec66a7
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-03-06 01:43:22 -0300

    Update home_page.dart

--- Flutter/curso16apps/agendacontatos/lib/ui/home_page.dart
+++ Flutter/curso16apps/agendacontatos/lib/ui/home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:agendacontatos/helpers/contact_helper.dart';
+import 'package:agendacontatos/ui/contact_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,12 +18,8 @@ class _HomePageState extends State<HomePage> {
     super.initState();
 
     //helper.saveContact();
-
-    helper.getAllContacts().then((list) {
-      setState() {
-        contacts = list;
-      }
-    });
+    _getAllContacts;
+    
   }
 
   @override
@@ -35,7 +32,10 @@ class _HomePageState extends State<HomePage> {
         ),
         backgroundColor: Colors.white,
         floatingActionButton: FloatingActionButton(
-            onPressed: () {},
+            //botao flutuante
+            onPressed: () {
+              _showContactPage();
+            },
             child: Icon(Icons.add),
             backgroundColor: Colors.red),
         body: ListView.builder(
@@ -48,46 +48,70 @@ class _HomePageState extends State<HomePage> {
 
   Widget _contactCard(BuildContext context, int index) {
     return GestureDetector(
-      child: Card(
-        child: Padding(
-          padding: EdgeInsets.all(10.0),
-          child: Row(
-            children: <Widget>[
-              Container(
-                width: 80.0,
-                height: 80.0,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                      image: contacts[index].imagem != null
-                          ? FileImage(File(contacts[index].imagem))
-                          : AssetImage("images/person.png")),
+        child: Card(
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width: 80.0,
+                  height: 80.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: contacts[index].imagem != null
+                            ? FileImage(File(contacts[index].imagem))
+                            : AssetImage("images/person.png")),
+                  ),
                 ),
-              ),
-              Padding(
-                  padding: EdgeInsets.only(left: 10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        contacts[index].nome ?? "",
-                        style: TextStyle(
-                            fontSize: 22.0, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        contacts[index].email ?? "",
-                        style: TextStyle(fontSize: 18.0),
-                      ),
-                      Text(
-                        contacts[index].telefone ?? "",
-                        style: TextStyle(fontSize: 18.0),
-                      )
-                    ],
-                  ))
-            ],
+                Padding(
+                    padding: EdgeInsets.only(left: 10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          contacts[index].nome ?? "",
+                          style: TextStyle(
+                              fontSize: 22.0, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          contacts[index].email ?? "",
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                        Text(
+                          contacts[index].telefone ?? "",
+                          style: TextStyle(fontSize: 18.0),
+                        )
+                      ],
+                    ))
+              ],
+            ),
           ),
         ),
-      ),
-    );
+        onTap: () {
+          _showContactPage(contact: contacts[index]);
+        });
+  }
+
+  void _showContactPage({Contact contact}) async {
+    final recContact = await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => ContactPage(
+                  contact: contact,
+                )));
+    if (recContact != null) {
+      if (contact != null) {
+        await helper.updateContact(recContact);
+        await 
+      }
+    }
+  }
+  _getAllContacts(){
+    helper.getAllContacts().then((list) {
+      setState() {
+        contacts = list;
+      }
+    });
   }
 }
