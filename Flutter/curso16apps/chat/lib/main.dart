e2f711768ba2172c001781417b2b0c558ed5d61a
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-03-09 22:32:15 -0300

    Comunicando com o Firebase e criado tema Dark

--- Flutter/curso16apps/chat/lib/main.dart
+++ Flutter/curso16apps/chat/lib/main.dart
@@ -13,6 +13,37 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return new MaterialApp(
+      title: 'Generated App',
+      theme: new ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF212121),
+        accentColor: const Color(0xFF64ffda),
+        canvasColor: const Color(0xFF303030),
+      ),
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('App Name'),
+      ),
+      body: new Container(
+        padding: const EdgeInsets.all(0.0),
+        alignment: Alignment.center,
+      ),
+    );
   }
 }
