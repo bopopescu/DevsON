c31e140058ea0d2ac2d01d6304a2b2063caf3f02
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-03-09 22:11:14 -0300

    Novo projeto de Chat

---
+++ Flutter/curso16apps/chat/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() {
+  runApp(MyApp());
+
+  Firestore.instance
+      .collection('col')
+      .document('doc')
+      .setData({'texto': 'daniel'});
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
