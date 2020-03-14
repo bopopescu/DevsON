86f8110b247083f8fda72a97424ff2180d660ea5
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-03-14 17:25:43 -0300

    Update text_composer.dart

--- Flutter/curso16apps/chat/lib/text_composer.dart
+++ Flutter/curso16apps/chat/lib/text_composer.dart
@@ -11,25 +11,26 @@ class _TextComposerState extends State<TextComposer> {
   @override
   Widget build(BuildContext context) {
     return Container(
+        margin: const EdgeInsets.symmetric(horizontal: 0),
         child: Row(
-      children: <Widget>[
-        IconButton(icon: Icon(Icons.photo_camera), onPressed: () {}),
-        Expanded(
-          child: TextField(
-              decoration:
-                  InputDecoration.collapsed(hintText: "Enviar uma mensagem"),
-              onChanged: (text) {
-                setState(() {
-                  _isComposing = text.isNotEmpty;
-                });
-              },
-              onSubmitted: (text) {}),
-        ),
-        IconButton(
-          icon: Icon(Icons.send),
-          onPressed: _isComposing ? () {} : null,
-        )
-      ],
-    ));
+          children: <Widget>[
+            IconButton(icon: Icon(Icons.photo_camera), onPressed: () {}),
+            Expanded(
+              child: TextField(
+                  decoration: InputDecoration.collapsed(
+                      hintText: "Enviar uma mensagem"),
+                  onChanged: (text) {
+                    setState(() {
+                      _isComposing = text.isNotEmpty;
+                    });
+                  },
+                  onSubmitted: (text) {}),
+            ),
+            IconButton(
+              icon: Icon(Icons.send),
+              onPressed: _isComposing ? () {} : null,
+            )
+          ],
+        ));
   }
 }
