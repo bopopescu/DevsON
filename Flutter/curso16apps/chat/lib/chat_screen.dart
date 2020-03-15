619ddc13f50231007c456679d840b2d7cc48686a
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-03-15 00:29:16 -0300

    Update chat_screen.dart

--- Flutter/curso16apps/chat/lib/chat_screen.dart
+++ Flutter/curso16apps/chat/lib/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:chat/text_composer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -8,6 +9,10 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  void _sendMessage(String text) {
+    Firestore.instance.collection("messages").add({'text': text});
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -15,7 +20,7 @@ class _ChatScreenState extends State<ChatScreen> {
         title: Text('App Name'),
         elevation: 0,
       ),
-      body: TextComposer((text) {}),
+      body: TextComposer(_sendMessage),
     );
   }
 }
