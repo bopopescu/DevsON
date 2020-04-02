e279145b7e2ff49d7418e9d9eb28fe81df8c5197
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-04-02 04:29:24 -0300

    Update chat_message.dart

--- Flutter/curso16apps/chat/lib/chat_message.dart
+++ Flutter/curso16apps/chat/lib/chat_message.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/material.dart';
 
 class ChatMessage extends StatelessWidget {
-  ChatMessage(this.data);
+  ChatMessage(this.data, this.mine);
 
   final Map<String, dynamic> data;
-
+  final bool mine;
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
       child: Row(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: CircleAvatar(
-              backgroundImage: NetworkImage(data['senderPhotoUrl']),
-            ),
-          ),
+          !mine
+              ? Padding(
+                  padding: const EdgeInsets.only(right: 16),
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(data['senderPhotoUrl']),
+                  ),
+                )
+              : Container(),
           Expanded(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment:
+                  mine ? CrossAxisAlignment.end : CrossAxisAlignment.start,
               children: <Widget>[
                 data['imgUrl'] != null
-                    ? Image.network(data['imgUrl'])
+                    ? Image.network(
+                        data['imgUrl'],
+                        width: 250,
+                      )
                     : Text(
                         data['text'],
                         style: TextStyle(fontSize: 16),
@@ -31,7 +37,15 @@ class ChatMessage extends StatelessWidget {
                     style: TextStyle(fontSize: 13, fontWeight: FontWeight.w500))
               ],
             ),
-          )
+          ),
+          mine
+              ? Padding(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(data['senderPhotoUrl']),
+                  ),
+                )
+              : Container(),
         ],
       ),
     );
