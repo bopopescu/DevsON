626119ec7cb922b5ffecc62649d1281b0999178d
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-04-02 04:07:36 -0300

    Dados do usuario

---
+++ Flutter/curso16apps/chat/lib/chat_message.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage(this.data);
+
+  final Map<String, dynamic> data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      child: Row(
+        children: <Widget>[
+          CircleAvatar(
+            backgroundImage: NetworkImage(data['senderPhotoUrl']),
+          )
+        ],
+      ),
+    );
+  }
+}
