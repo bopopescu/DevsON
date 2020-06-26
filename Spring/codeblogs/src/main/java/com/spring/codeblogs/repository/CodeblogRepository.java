ce96879eaf196ba8b5437f7f61c01c71fb6aef0e
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-06-26 14:48:05 -0300

    Removido do repositporio antigo

--- Spring/codeblogs/src/main/java/com/spring/codeblogs/repository/CodeblogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.codeblogs.repository;
-
-import com.spring.codeblogs.model.Post;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CodeblogRepository extends JpaRepository<Post, Long> {
-
-}
