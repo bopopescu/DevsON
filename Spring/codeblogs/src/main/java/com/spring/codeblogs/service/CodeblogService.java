9a7290f681bfb28b21bc1a5e39887e29c22a3248
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-06-25 22:26:41 -0300

    mostrar pro Spring que deve ser gerenciado por ele.

--- Spring/codeblogs/src/main/java/com/spring/codeblogs/service/CodeblogService.java
+++ Spring/codeblogs/src/main/java/com/spring/codeblogs/service/CodeblogService.java
@@ -3,9 +3,11 @@ package com.spring.codeblogs.service;
 import com.spring.codeblogs.model.Post;
 import com.spring.codeblogs.repository.CodeblogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+//mostrar pro Spring que deve ser gerenciado por ele.
+@Service
 public class CodeblogService implements CodeblogServiceInterface {
     //Ponto de Injeção do Repository (CodeBlogRepository) que já tem herança extendendo com os metodos prontos do JPA
     @Autowired
